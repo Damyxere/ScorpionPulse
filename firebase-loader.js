9c147676cea38fac21aa8568193eebfcccb56d41
scorpion <143728714+Damyxere@users.noreply.github.com>
2026-06-24 22:19:55 +0200

    Update firebase-loader.js

--- firebase-loader.js
+++ firebase-loader.js
@@ -1,2 +1,24 @@
+// CONFIGURAZIONE FIREBASE PROGETTO: SCORPION PULSE
+const firebaseConfig = {
+    apiKey: "AIzaSyAh0Mqbd_bamcTKda38tGU2H09W8rGnmzU",
+    authDomain: "scorpionpulse-b660f.firebaseapp.com",
+    projectId: "scorpionpulse-b660f",
+    storageBucket: "scorpionpulse-b660f.firebasestorage.app",
+    messagingSenderId: "832084644457",
+    appId: "1:832084644457:web:2ce04ba59f6271db57c8f8",
+    measurementId: "G-0RQ94CYRSJ"
+};
 
+// Inizializzazione globale dei servizi
+try {
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+        console.log("🔥 Firebase Scorpion Pulse inizializzato correttamente!");
+    }
+} catch (error) {
+    console.error("❌ Errore durante l'inizializzazione di Firebase:", error);
+}
+
+const auth = firebase.auth();
+const db = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
